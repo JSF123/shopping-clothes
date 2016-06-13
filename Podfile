26ac7af63edfc3657f079a38f6c5dc028a91f500
JingFengZi <m18339113690@163.com>
2016-06-13 22:43:07 +0800

    12
    123

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, "8.0"
+
+
+target :'ShoppingClothes' do
+  # pod 'Kiwi'
+   pod 'AFNetworking'
+   pod 'SDWebImage', '~> 3.7.6'
+   pod 'MJRefresh'
+end
---
+++ ShoppingClothes/View/GtandCustomView/CustomHeaderView.h
@@ -0,0 +1,23 @@
+//
+//  CustomHeaderView.h
+//  ShoppingClothes
+//
+//  Created by student on 16/6/9.
+//  Copyright © 2016年 jing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomHeaderView : UIView
+
+@property (nonatomic,strong)UIButton *countryBtn;
+
+//@property (nonatomic,strong)UIImageView *_leftImage;
+//
+//@property (nonatomic,strong)UIImageView *_rightImage;
+//
+//@property (nonatomic,strong)UILabel *_titleLabel;
+
+//- (void)getCustomHeaderTitle:(NSString *)titleLabel  WithRightImage:(NSString *)rightImage WithLeftImage:(NSString *)leftImage;
+
+@end
